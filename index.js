df5029f2ea9be862f1cfe5ae9dc553c862a59f47
thummel1867 <thummel1867@gmail.com>
2022-10-24 10:23:44 -0400

    updates cors

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.redirect('/emissions');
